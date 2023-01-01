e468a7b17364f42b41a6e8c7211825eb92401eda
Pratik Nayak <104261131+pratikn2008@users.noreply.github.com>
2023-01-01 11:33:50 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 img = "";
 Status = "";
+objects = [];
 
 function setup() {
-    canvas = createCanvas(640, 420);
+    canvas = createCanvas(380, 380);
     canvas.center();
-    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
-    document.getElementById("status").innerHTML = "Status :  Detecting Objects";
+    video = createCapture(VIDEO);
+    video.size(380, 380);
+    video.hide();
 }
 
 function preload() {
@@ -13,30 +15,43 @@ function preload() {
 }
 
 function draw() {
-    image(img, 0, 0, 640, 240);
-    fill("#FF0000");
-    text("Dog", 45, 75);
-    noFill();
-    stroke("#FF0000");
-    rect(30, 60, 450, 350);
+    image(video, 0, 0, 380, 380);
 
-    fill("#FF0000");
-    text("Cat", 320, 120);
-    noFill();
-    stroke("#FF0000");
-    rect(300, 90, 270, 320);
+    if(Status != "")
+    {
+        r = random(255);
+        g = random(255);
+        b = random(255);
+        objectDetector.detect(video, gotResult);
+        for(i = 0; i < objects.length; i++);
+        {
+            document.getElementById("status").innerHTML = "Status : Object Detected";
+            document.getElementById("number_of_objects").innerHTML = "Number Of Objects Deteccted are : " + objects.length;
+
+            fill(r, g, b);
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke(r, g, b);
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height); 
+        }
+    }
 }
 
 function modelLoaded() {
     console.log("Model Loaded!");
     Status = true;
-    objectDetector.detect(img, gotResult);
 }
 
 function gotResult(error, results) {
     if(error){
         console.log(error);
     }
-    console.log(results)
+    console.log(results);
+    objects = results;
 }
 
+function start(){
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status :  Detecting Objects";
+}
